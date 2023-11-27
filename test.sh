0a5d5f65ec1de4f29aec24904901c75047eec92f
just-hms <alessandroversari1@gmail.com>
2023-11-27 09:49:17 +0100

    edit: the test template is now a working .c file

--- test.sh
+++ test.sh
@@ -1,78 +1,6 @@
 #!/bin/bash
 
-template='
-#include <stdio.h>
-#include <pthread.h>
-
-int testsResult = 0;
-// Declare the mutex;
-pthread_mutex_t mutex; 
-
-typedef struct {
-    int (*testFunc)();
-    char *funcName;
-} TestStruct;
-
-// Function to run tests
-void *run_test(void *arg)
-{
-    TestStruct *test = (TestStruct *)arg; // Cast argument to TestStruct pointer
-    int res = test->testFunc();
-    pthread_mutex_lock(&mutex);
-    testsResult = testsResult || res;
-    pthread_mutex_unlock(&mutex);
-
-	if (res == 0) {
-        printf("%-20s\t[OK]\n", test->funcName);
-    } else {
-        fprintf(stderr, "\033[31m%-20s\t[FAILED]\033[0m\n", test->funcName);
-    }
-    return NULL;
-}
-
-#define NUM_TESTS {@NUM_TESTS}
-#define NUM_THREADS {@NUM_THREADS}
-
-{@INCLUDES}
-
-int main()
-{
-	pthread_t threads[NUM_THREADS];
-	pthread_mutex_init(&mutex, NULL); // Initialize the mutex	
-
-	TestStruct tests[NUM_TESTS] = {{@TESTS}};
-	int threadIndex = 0;
-
-	// Create and join threads
-	for (int i = 0; i < NUM_TESTS; i++)
-	{
-		if (pthread_create(&threads[threadIndex], NULL, run_test, &tests[i]))
-		{
-			fprintf(stderr, "Error creating thread\n");
-			return 1;
-		}
-
-		if (++threadIndex >= NUM_THREADS)
-		{
-			// Wait for threads to finish
-			for (int j = 0; j < NUM_THREADS; j++)
-			{
-				pthread_join(threads[j], NULL);
-			}
-			threadIndex = 0;
-		}
-	}
-
-	// Join any remaining threads
-	for (int i = 0; i < threadIndex; i++)
-	{
-		pthread_join(threads[i], NULL);
-	}
-
-	pthread_mutex_destroy(&mutex); // Destroy the mutex after use
-	return testsResult;
-}
-'
+template=$(cat test_template.c)
 
 # Initialize arrays for includes and function calls
 includes=()
@@ -102,10 +30,10 @@ tests=$(IFS=, ; echo "${calls[*]}")
 includes=$(IFS=$'\n' ; echo "${includes[*]}")
 
 # Update the template
-template=${template//'{@NUM_TESTS}'/$num_tests}
-template=${template//'{@NUM_THREADS}'/$num_threads}
-template=${template//'{@TESTS}'/$tests}
-template=${template//'{@INCLUDES}'/$includes}
+template=${template//'/*{@NUM_TESTS}*/'/"+$num_tests"}
+template=${template//'/*{@NUM_THREADS}*/'/"+$num_threads"}
+template=${template//'/*{@TESTS}*/'/$tests}
+template=${template//'/*{@INCLUDES}*/'/$includes}
 
 # Print the updated template
 echo "$template" | while IFS= read -r line; do
